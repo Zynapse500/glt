a3a3cdab7921c42433eae91c09be2788b61c9dad
Christofer Nolander <christofer.nolander@gmail.com>
2017-12-29 02:05:27 +0100

    Fixed silly rounding issue

--- include/vec/vec_core.hpp
+++ include/vec/vec_core.hpp
@@ -18,18 +18,18 @@ namespace glt {
     ///////////////////////////
     // a . b = |a||b| cos(theta)
     ///////////////////////////
-    template <class T>
-    inline T dot(const vec2<T>& a, const vec2<T>& b) {
+    template<class T>
+    inline T dot(const vec2<T> &a, const vec2<T> &b) {
         return a.x * b.x + a.y * b.y;
     }
 
-    template <class T>
-    inline T dot(const vec3<T>& a, const vec3<T>& b) {
+    template<class T>
+    inline T dot(const vec3<T> &a, const vec3<T> &b) {
         return a.x * b.x + a.y * b.y + a.z * b.z;
     }
 
-    template <class T>
-    inline T dot(const vec4<T>& a, const vec4<T>& b) {
+    template<class T>
+    inline T dot(const vec4<T> &a, const vec4<T> &b) {
         return a.x * b.x + a.y * b.y + a.z * b.z + a.w * b.w;
     }
 
@@ -42,63 +42,63 @@ namespace glt {
     // a . a = |a||a|
     ///////////////////////////
 
-    template <class T>
-    inline T squaredLength(const vec2<T>& a) {
+    template<class T>
+    inline T squaredLength(const vec2<T> &a) {
         return dot(a, a);
     }
 
-    template <class T>
-    inline T squaredLength(const vec3<T>& a) {
+    template<class T>
+    inline T squaredLength(const vec3<T> &a) {
         return dot(a, a);
     }
 
-    template <class T>
-    inline T squaredLength(const vec4<T>& a) {
+    template<class T>
+    inline T squaredLength(const vec4<T> &a) {
         return dot(a, a);
     }
 
 
     // Return the length (magnitude) of a vector
-    template <class T>
-    inline T length(const vec2<T>& a) {
+    template<class T>
+    inline T length(const vec2<T> &a) {
         return sqrt(squaredLength(a));
     }
 
-    template <class T>
-    inline T length(const vec3<T>& a) {
+    template<class T>
+    inline T length(const vec3<T> &a) {
         return sqrt(squaredLength(a));
     }
 
-    template <class T>
-    inline T length(const vec4<T>& a) {
+    template<class T>
+    inline T length(const vec4<T> &a) {
         return sqrt(squaredLength(a));
     }
 
 
     // Return the normalized form of this vector
-    template <class T>
-    inline vec2<T> normalize(const vec2<T>& a) {
+    template<class T>
+    inline vec2<T> normalize(const vec2<T> &a) {
         return a / length(a);
     }
 
-    template <class T>
-    inline vec3<T> normalize(const vec3<T>& a) {
+    template<class T>
+    inline vec3<T> normalize(const vec3<T> &a) {
         return a / length(a);
     }
 
-    template <class T>
-    inline vec4<T> normalize(const vec4<T>& a) {
+    template<class T>
+    inline vec4<T> normalize(const vec4<T> &a) {
         return a / length(a);
     }
 
 
     // Return the cross product of two vectors
-    template <class T>
-    inline vec3<T> cross(const vec3<T>& a, const vec3<T>& b) {
+    template<class T>
+    inline vec3<T> cross(const vec3<T> &a, const vec3<T> &b) {
         return {
-                a.y*b.z - a.z*b.y,
-                a.z*b.x - a.x*b.z,
-                a.x*b.y - a.y*b.x
+                a.y * b.z - a.z * b.y,
+                a.z * b.x - a.x * b.z,
+                a.x * b.y - a.y * b.x
         };
     }
 
@@ -106,71 +106,89 @@ namespace glt {
     // Implement rounding for vectors
 
     // Round downward
-    template <class T>
-    vec2<T> floor(const vec2<T>& v) {
-        v.x = ::floor((v.x));
-        v.y = ::floor((v.y));
+    template<class T>
+    vec2<T> floor(const vec2<T> &v) {
+        return {
+                ::floor((v.x)),
+                ::floor((v.y))
+        };
     }
 
-    template <class T>
-    vec3<T> floor(const vec3<T>& v) {
-        v.x = ::floor((v.x));
-        v.y = ::floor((v.y));
-        v.z = ::floor((v.z));
+    template<class T>
+    vec3<T> floor(const vec3<T> &v) {
+        return {
+                ::floor((v.x)),
+                ::floor((v.y)),
+                ::floor((v.z)),
+        }
     }
 
-    template <class T>
-    vec4<T> floor(const vec4<T>& v) {
-        v.x = ::floor((v.x));
-        v.y = ::floor((v.y));
-        v.z = ::floor((v.z));
-        v.w = ::floor((v.w));
+    template<class T>
+    vec4<T> floor(const vec4<T> &v) {
+        return {
+                ::floor((v.x)),
+                ::floor((v.y)),
+                ::floor((v.z)),
+                ::floor((v.w))
+        };
     }
 
 
     // Round to nearest whole number
-    template <class T>
-    vec2<T> round(const vec2<T>& v) {
-        v.x = ::round((v.x));
-        v.y = ::round((v.y));
+    template<class T>
+    vec2<T> round(const vec2<T> &v) {
+        return {
+                ::round((v.x)),
+                ::round((v.y))
+        };
     }
 
-    template <class T>
-    vec3<T> round(const vec3<T>& v) {
-        v.x = ::round((v.x));
-        v.y = ::round((v.y));
-        v.z = ::round((v.z));
+    template<class T>
+    vec3<T> round(const vec3<T> &v) {
+        return {
+                ::round((v.x)),
+                ::round((v.y)),
+                ::round((v.z))
+        };
     }
 
-    template <class T>
-    vec4<T> round(const vec4<T>& v) {
-        v.x = ::round((v.x));
-        v.y = ::round((v.y));
-        v.z = ::round((v.z));
-        v.w = ::round((v.w));
+    template<class T>
+    vec4<T> round(const vec4<T> &v) {
+        return {
+                ::round((v.x)),
+                ::round((v.y)),
+                ::round((v.z)),
+                ::round((v.w))
+        };
     }
 
 
     // Round upward
-    template <class T>
-    vec2<T> ceil(const vec2<T>& v) {
-        v.x = ::ceil((v.x));
-        v.y = ::ceil((v.y));
+    template<class T>
+    vec2<T> ceil(const vec2<T> &v) {
+        return {
+                ::ceil((v.x)),
+                ::ceil((v.y))
+        };
     }
 
-    template <class T>
-    vec3<T> ceil(const vec3<T>& v) {
-        v.x = ::ceil((v.x));
-        v.y = ::ceil((v.y));
-        v.z = ::ceil((v.z));
+    template<class T>
+    vec3<T> ceil(const vec3<T> &v) {
+        return {
+                ::ceil((v.x)),
+                ::ceil((v.y)),
+                ::ceil((v.z))
+        };
     }
 
-    template <class T>
-    vec4<T> ceil(const vec4<T>& v) {
-        v.x = ::ceil((v.x));
-        v.y = ::ceil((v.y));
-        v.z = ::ceil((v.z));
-        v.w = ::ceil((v.w));
+    template<class T>
+    vec4<T> ceil(const vec4<T> &v) {
+        return {
+                ::ceil((v.x)),
+                ::ceil((v.y)),
+                ::ceil((v.z)),
+                ::ceil((v.w))
+        };
     }
 }
 
